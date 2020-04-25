7204808332abb00a54201ec6e204a7a5ea76a0eb
Hongchao Ma <hongchao.ma@synaptop.com>
2020-04-25 17:41:50 -0400

    add eks-creation.sh

---
+++ eks-creation.sh
@@ -0,0 +1,12 @@
+eksctl create cluster \
+--name prod \
+--region us-east-1 \
+--nodegroup-name workers \
+--node-type t3.medium \
+--nodes 1 \
+--nodes-min 1 \
+--nodes-max 3 \
+--ssh-access \
+--ssh-public-key eks-dev \
+--zones=us-east-1a,us-east-1b \
+--managed
